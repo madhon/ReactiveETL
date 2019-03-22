705bea38f9c0b1a6c54e8b35eba452ce2246d161
Jonathon Douglas <jdouglas@acquisinsurance.com>
2019-03-22 12:00:00 +0000

    db set

---
+++ ReactiveETL/Infrastructure/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ReactiveETL.Infrastructure
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string ProviderName { get; set; }
+    }
+}
